85088bdd331beaa4be551c9835cdaab0ea1844ec
Makus-Moeller <79902310+Makus-Moeller@users.noreply.github.com>
2022-05-26 13:38:17 +0200

    First hash function

--- Main_Program.fs
+++ Main_Program.fs
@@ -1,8 +1,9 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
 
-let add_five (x : int) : int = 
-    x+5
+let multiply_shift_hashing (x:uint64) (a:uint64) (l: int) : uint64 = 
+    (a*x) >>> (64-l)
 
-let c = add_five 4
-printfn "%A" c
+let hashvalue = multiply_shift_hashing 34921UL 64244UL 35 
+
+printfn "%A" hashvalue
