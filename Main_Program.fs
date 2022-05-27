7f1ddb7a1836469f999541f10d309c958c7a3732
unknown <jakobkarrer@gmail.com>
2022-05-27 13:11:58 +0200

    Random addded

--- Main_Program.fs
+++ Main_Program.fs
@@ -3,6 +3,11 @@
 exception InnerError of string
 
 open System
+let prime_p : bigint = (bigint 2<<<89)-bigint 1
+let rand_numb_bigint: bigint list = [
+    961026785849338145764820I
+    320523630964915694060580I
+] 
 
 
 //Assignment 1 a
@@ -13,7 +18,6 @@ let hashvalue = multiply_shift_hashing 34921UL 64244UL 35
 //printfn "%A" hashvalue
 
 //Assignment 1 b
-let prime_p : bigint = (bigint 2<<<89)-bigint 1
 
 let  multiply_mod_prime (a: bigint) (b: bigint) (x:uint64) (l: int32) : bigint = 
     if a >= prime_p || b >= prime_p then raise(InnerError("To LARGE A or B")) 
@@ -21,17 +25,20 @@ let  multiply_mod_prime (a: bigint) (b: bigint) (x:uint64) (l: int32) : bigint =
         let z : bigint = (a*bigint x + b)  //Big multiplication
         printfn "z value: %A" z
         let y = (z&&&prime_p) + (z>>>89)
-        printfn "y value: %A" y
         let power_l = bigint 1<<<l
+        printfn "y value: %A" y
         if y >= prime_p then  
             (y-prime_p) % power_l //Jeg har ændret denne shift
         else 
             y % power_l
-            
-let a : bigint = 34921432462I
-let b : bigint = 56783642446I
 
 
-let hashvalue2 = multiply_mod_prime a b 403232UL 11 
-printfn "%A" hashvalue2 
+[<EntryPoint>]
+let main argv =       
+    let a : bigint = 349214000000073I
+    let b : bigint = 560000000867I
+    let hashvalue2 = multiply_mod_prime a b 403232UL 11
+    0
+
+
 
